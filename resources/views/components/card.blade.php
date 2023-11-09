6ead610d4412ac0320a521644540c8a4baa5cf20
Riyan Amanda <ryant.n92@gmail.com>
2023-11-09 15:52:54 +0700

    feat: add responsive and minimal design

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,65 +1,56 @@
 @props(['title', 'kapasitas', 'terpakai', 'kelas1', 'kelas2', 'kelas3', 'vip'])
 
-<div class="p-5 rounded-lg bg-white text-gray-600 shadow-md">
-    <div class="text-center mb-5">
-        <div class="text-2xl font-bold font-loto tracking-widest">
-            {{ $title }}
-        </div>
-        <div class="text-3xl font-medium text-gray-800">
-            {{ $kapasitas - $terpakai }}
+<div class="p-5 rounded-lg bg-white text-gray-600 border-y-8">
+    <div class="flex items-center justify-between mb-10">
+        <span class="text-xl font-semibold tracking-widest leading-relaxed">{{ $title }}</span>
+        <div class="flex flex-col items-end">
+            <span class="text-gray-400 text-sm">Tersedia</span>
+            <span class="text-gray-600 font-medium">
+                {{ $kapasitas - $terpakai }}
+                <i class="fa fa-bed ml-1"></i>
+            </span>
         </div>
     </div>
 
-    <div class="flex justify-between mb-5 text-center text-lg">
+    <div class="flex justify-between text-center text-sm">
         @isset($kelas1)
             <div>
-                <div>KELAS 1</div>
-                <div class="text-gray-800 font-medium">
-                    {{ $kelas1 }}
+                <div class="mb-1 text-md text-gray-400">KELAS 1</div>
+                <div class="flex items-center border py-1 px-3 rounded-lg space-x-3">
+                    <span class="font-medium text-xl">{{ $kelas1->jumlah_bed - $kelas1->digunakan }}</span>
+                    <i class="fas fa-bed"></i>
                 </div>
             </div>
         @endisset
 
         @isset($kelas2)
             <div>
-                <div>KELAS 2</div>
-                <div class="text-gray-800 font-medium">
-                    {{ $kelas2 }}
+                <div class="mb-1 text-md text-gray-400">KELAS 2</div>
+                <div class="flex items-center border py-1 px-3 rounded-lg space-x-3">
+                    <span class="font-medium text-xl">{{ $kelas2->jumlah_bed - $kelas2->digunakan }}</span>
+                    <i class="fas fa-bed"></i>
                 </div>
             </div>
         @endisset
 
         @isset($kelas3)
             <div>
-                <div>KELAS 3</div>
-                <div class="text-gray-800 font-medium">
-                    {{ $kelas3 }}
+                <div class="mb-1 text-md text-gray-400">KELAS 3</div>
+                <div class="flex items-center border py-1 px-3 rounded-lg space-x-3">
+                    <span class="font-medium text-xl">{{ $kelas3->jumlah_bed - $kelas3->digunakan }}</span>
+                    <i class="fas fa-bed"></i>
                 </div>
             </div>
         @endisset
 
         @isset($vip)
             <div>
-                <div>VIP</div>
-                <div class="text-gray-800 font-medium">
-                    {{ $vip }}
+                <div class="mb-1 text-md text-gray-400">VIP</div>
+                <div class="flex items-center border py-1 px-3 rounded-lg space-x-3">
+                    <span class="font-medium text-xl">{{ $vip->jumlah_bed - $vip->digunakan }}</span>
+                    <i class="fas fa-bed"></i>
                 </div>
             </div>
         @endisset
-    </div>
-
-    <div class="flex justify-between items-center">
-        <div>
-            <span>Kapasitas</span>
-            <span class="text-gray-800 font-medium">
-                {{ $kapasitas ?? 0 }}
-            </span>
-        </div>
-        <div>
-            <span>Terpakai</span>
-            <span class="text-gray-800 font-medium">
-                {{ $terpakai ?? 0 }}
-            </span>
-        </div>
     </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,43 +19,44 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="px-20 antialiased h-full bg-gray-100 font-roboto">
-    <header class="py-5 w-6/12 mb-10">
-        <h1 class="text-4xl font-medium mb-5 leading-tight">Informasi Ketersediaan Kapasitas Tempat Tidur Rawat Inap
+<body class="px-4 lg:px-20 antialiased h-full bg-gray-100 font-roboto">
+    <header class="py-10 lg:w-6/12 lg:mb-10">
+        <h1 class="text-lg md:text-4xl font-medium mb-5 leading-tight">Informasi Ketersediaan Kapasitas Tempat Tidur
+            Rawat Inap
             RS Ernaldi Bahar
         </h1>
 
-        <div class="font-medium tracking-wider divide-x-2 divide-gray-500">
-            <span class="pr-3">Tanggal : <span class="font-black">
+        <div class="text-sm lg:text-md font-medium tracking-wider text-gray-500 divide-x-2 divide-gray-500">
+            <span class="pr-3">Tanggal : <span class="font-black text-gray-900">
                     {{ now()->format('d M Y') }}
                 </span></span>
-            <span class="pl-5">Pukul : <span class="font-black">
+            <span class="pl-5">Pukul : <span class="font-black text-gray-900">
                     {{ now()->format('H:m:s') }}
                 </span></span>
         </div>
     </header>
 
     <main class="w-full">
-        <div class="grid grid-cols-4 gap-12 mb-20">
+        <div class="grid lg:grid-cols-4 lg:gap-12 mb-10 lg:mb-28">
             {{-- ASOKA --}}
-            <x-card title="ASOKA" :kelas1="$asoka1->jumlah_bed" :kelas2="$asoka2->jumlah_bed" :kelas3="$asoka3->jumlah_bed" :vip="$asokavip->jumlah_bed"
+            <x-card title="ASOKA" :kelas1="$asoka1" :kelas2="$asoka2" :kelas3="$asoka3" :vip="$asokavip"
                 :kapasitas="$asoka1->jumlah_bed + $asoka2->jumlah_bed + $asoka3->jumlah_bed + $asokavip->jumlah_bed" :terpakai="$asoka1->digunakan + $asoka2->digunakan + $asoka3->digunakan + $asokavip->digunakan" />
 
             {{-- BANGAU --}}
-            <x-card title="BANGAU" :kelas3="$bangau3->jumlah_bed" :kapasitas="$bangau3->jumlah_bed" :terpakai="$bangau3->digunakan" />
+            <x-card title="BANGAU" :kelas3="$bangau3" :kapasitas="$bangau3->jumlah_bed" :terpakai="$bangau3->digunakan" />
 
             {{-- CAMAR --}}
-            <x-card title="CAMAR" :kelas3="$camar3->jumlah_bed" :kapasitas="$camar3->jumlah_bed" :terpakai="$camar3->digunakan" />
+            <x-card title="CAMAR" :kelas3="$camar3" :kapasitas="$camar3->jumlah_bed" :terpakai="$camar3->digunakan" />
 
             {{-- CEMPAKA --}}
-            <x-card title="CEMPAKA" :kelas1="$cempaka1->jumlah_bed" :kelas2="$cempaka2->jumlah_bed" :kelas3="$cempaka3->jumlah_bed" :vip="$cempakavip->jumlah_bed"
+            <x-card title="CEMPAKA" :kelas1="$cempaka1" :kelas2="$cempaka2" :kelas3="$cempaka3" :vip="$cempakavip"
                 :kapasitas="$cempaka1->jumlah_bed +
                     $cempaka2->jumlah_bed +
                     $cempaka3->jumlah_bed +
                     $cempakavip->jumlah_bed" :terpakai="$cempaka1->digunakan + $cempaka2->digunakan + $cempaka3->digunakan + $cempakavip->digunakan" />
 
             {{-- CENDRAWASIH --}}
-            <x-card title="CENDRAWASIH" :kelas1="$cendrawasih1->jumlah_bed" :kelas2="$cendrawasih2->jumlah_bed" :kelas3="$cendrawasih3->jumlah_bed" :vip="$cendrawasihvip->jumlah_bed"
+            <x-card title="CENDRAWASIH" :kelas1="$cendrawasih1" :kelas2="$cendrawasih2" :kelas3="$cendrawasih3" :vip="$cendrawasihvip"
                 :kapasitas="$cendrawasih1->jumlah_bed +
                     $cendrawasih2->jumlah_bed +
                     $cendrawasih3->jumlah_bed +
@@ -65,21 +66,22 @@
                     $cendrawasihvip->digunakan" />
 
             {{-- KENANGA --}}
-            <x-card title="KENANGA" :kelas3="$kenanga3->jumlah_bed" :kapasitas="$kenanga3->jumlah_bed" :terpakai="$kenanga3->digunakan" />
+            <x-card title="KENANGA" :kelas3="$kenanga3" :kapasitas="$kenanga3->jumlah_bed" :terpakai="$kenanga3->digunakan" />
 
             {{-- MERAK --}}
-            <x-card title="MERAK" :kelas3="$merak3->jumlah_bed" :kapasitas="$merak3->jumlah_bed" :terpakai="$merak3->digunakan" />
+            <x-card title="MERAK" :kelas3="$merak3" :kapasitas="$merak3->jumlah_bed" :terpakai="$merak3->digunakan" />
 
             {{-- MERPATI --}}
-            <x-card title="MERPATI" :kelas3="$merpati3->jumlah_bed" :kapasitas="$merpati3->jumlah_bed" :terpakai="$merpati3->digunakan" />
+            <x-card title="MERPATI" :kelas3="$merpati3" :kapasitas="$merpati3->jumlah_bed" :terpakai="$merpati3->digunakan" />
         </div>
 
-        <div class="flex justify-end text-xl divide-x-2 divide-gray-500 tracking-widest">
-            <p class="pr-5">
+        <div
+            class="flex justify-between lg:justify-end text-xs lg:text-xl lg:divide-x-2 divide-gray-500 tracking-widest">
+            <p class="lg:pr-5 mb-10 lg:mb-0">
                 Total Tersedia <span
                     class="font-medium border p-4 rounded bg-green-700 text-white shadow-md">{{ $totalBed }}</span>
             </p>
-            <p class="pl-5">
+            <p class="lg:pl-5">
                 Total Terpakai <span
                     class="font-medium border p-4 rounded bg-gray-700 text-white shadow-md">{{ $totalTerpakai }}</span>
             </p>
